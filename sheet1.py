63643451a0a4f0ce29d89180b299db209ad71668
lukasredfield <lukasredfield@gmail.com>
2022-11-26 12:43:02 -0300

    save 02

--- sheet1.py
+++ sheet1.py
@@ -4,19 +4,13 @@ import pandas as pd
 import csv
 
 
-Autores = [
-    ['Joseluna','Positive','Ecuador','PA'],['sixt93','Negative','Colombia','CA'],['alberto','Positive','Francia','OT'],['carlosz','Negative','USA','OT'],['jgbesq','Negative','UK','PA'],
-    ['nuri90','Neutral','Spain','EV'],['santiago8','Neutral','Chile','KS'],['Joseluna','Positive','Peru','KS'],['alberto','Positive','Francia','OT'],['jgbesq','Negative','Panana','KS'],
-    ['sixt93','Negative','Portugal','OT'],['santiago8','Neutral','Chile','KS'],['santiago8','Neutral','Bolivia','PA'],['nico23','Neutral','USA','PA'],['alberto','Positive','USA','KS'],
-]
+df = pd.read_csv('database.csv')
 
-Dic_Autores = { '1':Autores[0],'2':Autores[1],'3':Autores[2],'4':Autores[3],'5':Autores[4],'6':Autores[5],'7':Autores[6],'8':Autores[7],'9':Autores[8],'10':Autores[9],'11':Autores[10],'12':Autores[11],'13':Autores[12],'14':Autores[13],'15':Autores[14],
 
-}
-
+pivot = pd.pivot_table(df,index=['Theme','Country','Country'], values=['Country'], columns=['Author'],)
 
 # Create DataFrame
-data = pd.DataFrame(Dic_Autores)
+data = pd.DataFrame(pivot)
  
 # Write to CSV file
 data.to_csv("Data_del_reto.csv")
--- sheet2.py
+++ sheet2.py
@@ -20,28 +20,17 @@ sheet.share('lukasredfield@gmail.com', perm_type='user', role='writer')
 sheet = cliente.open("Reto 1 - sheet").sheet1
 
 
-Autores = [
-    ['Joseluna','Positive','Ecuador','PA'],['sixt93','Negative','Colombia','CA'],['alberto','Positive','Francia','OT'],['carlosz','Negative','USA','OT'],['jgbesq','Negative','UK','PA'],
-    ['nuri90','Neutral','Spain','EV'],['santiago8','Neutral','Chile','KS'],['Joseluna','Positive','Peru','KS'],['alberto','Positive','Francia','OT'],['jgbesq','Negative','Panana','KS'],
-    ['sixt93','Negative','Portugal','OT'],['santiago8','Neutral','Chile','KS'],['santiago8','Neutral','Bolivia','PA'],['nico23','Neutral','USA','PA'],['alberto','Positive','USA','KS'],
-]
-
-Dic_Autores = { '1':Autores[0],'2':Autores[1],'3':Autores[2],'4':Autores[3],'5':Autores[4],'6':Autores[5],'7':Autores[6],'8':Autores[7],'9':Autores[8],'10':Autores[9],'11':Autores[10],'12':Autores[11],'13':Autores[12],'14':Autores[13],'15':Autores[14],
-
-}
-
-
 df = pd.read_csv('Data_del_reto.csv')
 
+df.fillna('',inplace=True)
 
 sheet.update([df.columns.values.tolist()]+ df.values.tolist())
 
+# sheet.update([df.columns.values.tolist()]+ df.values.tolist())
+
+# pivot = pd.pivot_table(df,index=['Author','Sentiment'], columns=['d'])
 
 
-
-# tabla = sheet2.pivot_table(columns=[Author,Sentiment],)
-
-#tabla = pd.pivot_table(df,index=['Author','Sentiment'], columns=['d'])
 
 
 
